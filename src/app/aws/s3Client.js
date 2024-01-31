dba8c8602c7be8d541068886a19ab21c5ab1cb55
mh2rashi <hamza022697@gmail.com>
2024-01-31 15:14:18 -0500

    Updated ACCESS KEY names for deployment

--- src/app/aws/s3Client.js
+++ src/app/aws/s3Client.js
@@ -5,8 +5,8 @@ const s3Client = () => {
     return new S3Client({
         region: 'us-east-2',
         credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+            secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
         },
     });
 };
--- src/app/aws/transcribeClient.js
+++ src/app/aws/transcribeClient.js
@@ -8,8 +8,8 @@ const transcribeClient = () => {
         region: 'us-east-2',
         credentials: {
 
-            accessKeyId: process.env.AWS_ACCESS_KEY,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+            secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
         },
 
     });
